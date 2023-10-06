c6bd5c41ee432b643e026f5e2b9be2bca35cb5e9
te70 <tevinkinuthia@gmail.com>
2023-10-06 12:02:38 +0400

    Add init files

---
+++ Part_1/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bRoutes = require('./routes/bRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// app.use('/api/blog', bRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
