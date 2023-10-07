6cbad8a03eaef752bcf82d351beca4c11e3003f4
te70 <tevinkinuthia@gmail.com>
2023-10-07 07:27:20 +0400

    Add changes

--- Part_1/app.js
+++ Part_1/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use('/api/blog', bRoutes);
+app.use('/api/blog', bRoutes.addBlogPost); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
